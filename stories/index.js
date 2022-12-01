66067fc6ee994f6e559632282c5379fc63eaaa17
Darren Lin <darrenlin815@hotmail.com>
2022-11-30 23:32:59 -0800

    Added the Form component and its story tests

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show.js'
 import Confirm from 'components/Appointment/Confirm.js'
 import Status from 'components/Appointment/Status.js'
 import Error from 'components/Appointment/Error.js'
+import Form from 'components/Appointment/Form.js'
 
 storiesOf('Button', module)
   .addParameters({
@@ -176,5 +177,21 @@ storiesOf('Appointment', module)
   <Error 
     message='Could not delete appointment.' 
     onClose={action('onClose')}
+  />
+))
+.add('Edit form', () => (
+  <Form
+    name='Lydia Miller-Jones'
+    interviewers={interviewers}
+    interviewer={3}
+    onSave={action('onSave')}
+    onCancel={action('onCancel')}
+  />
+))
+.add('Create form', () => (
+  <Form
+    interviewers={interviewers}
+    onSave={action('onSave')}
+    onCancel={action('onCancel')}
   />
 ))
