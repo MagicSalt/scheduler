72cbff4e8c97901f2fac9b425e1d10e55e1723d2
Darren Lin <darrenlin815@hotmail.com>
2022-12-01 00:18:25 -0800

    Added Appointments component to Application.js with some fake data

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.scss'
 import Header from './Header';
 import Show from './Show';
-import Empty from './Empty'
+import Empty from './Empty';
 
 export default function Appointment(props) {
   return (
