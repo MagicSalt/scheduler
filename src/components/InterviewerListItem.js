e435faeeffd2ba3308673cae48ba1309b5a55814
Darren Lin <darrenlin815@hotmail.com>
2022-11-30 17:49:13 -0800

    Fixed onClick to return interviewer id instead of name

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -11,7 +11,7 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
         className={interviewerImgClass}
         src={props.avatar}
